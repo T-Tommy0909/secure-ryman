da7f435e3ddc144b70e419baaff12a25fa955f20
T-Tommy0909 <tomi0909tomisawa@gmail.com>
2024-07-16 10:49:39 +0000

    feat: Add Auth0 configuration and user provider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <UserProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </UserProvider>
   );
 }
