8e27ad4e2ce664141b47556f81b3fd4ac04f34f4
T-Tommy0909 <tomi0909tomisawa@gmail.com>
2024-08-14 23:59:57 +0000

    feat: Add myAnswerList API endpoint for retrieving user's answers

--- src/server/routers/answer.ts
+++ src/server/routers/answer.ts
@@ -1,8 +1,62 @@
 import { router, procedure } from "@/server/trpc";
 import { prisma } from "@/server/utils/prisma";
 import { CreateAnswersRequestSchema } from "@/types/answer";
+import { z } from "zod";
 
 export const answerRouter = router({
+  myAnswerList: procedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const myAns = await prisma.part.findMany({
+        include: {
+          categorys: {
+            include: {
+              answers: {
+                where: {
+                  userId: input.userId,
+                },
+              },
+            },
+          },
+        },
+      });
+      return await Promise.all(
+        myAns.map(async (part) => {
+          return {
+            partName: part.name,
+            category: await Promise.all(
+              part.categorys.map(async (category) => {
+                return {
+                  categoryName: category.name,
+                  points:
+                    (
+                      await Promise.all(
+                        category.answers.map(async (answer) => {
+                          const answerChoice =
+                            await prisma.answerChoice.findUnique({
+                              where: {
+                                id: answer.answerChoiceId,
+                              },
+                              select: {
+                                points: true,
+                              },
+                            });
+                          if (!answerChoice) {
+                            throw new Error("AnswerChoice not found");
+                          }
+                          return answerChoice.points;
+                        }),
+                      )
+                    ).reduce((sum, points) => sum + points, 0) /
+                    category.answers.length,
+                };
+              }),
+            ),
+          };
+        }),
+      );
+    }),
+
   createMany: procedure
     .input(CreateAnswersRequestSchema)
     .mutation(async ({ input }) => {
