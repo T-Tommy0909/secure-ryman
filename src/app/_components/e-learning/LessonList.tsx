2eb69a6a30676bf1ac188959ca1e61197fa1a0a2
T-Tommy0909 <tomi0909tomisawa@gmail.com>
2024-10-05 09:11:53 +0000

    feat: Add logic in e-learning page

--- src/app/_components/e-learning/LessonList.tsx
+++ src/app/_components/e-learning/LessonList.tsx
@@ -1,54 +1,14 @@
 import { FC } from "react";
+import { serverApi } from "@/app/_trpc/server-api";
 import { BookOpen } from "lucide-react";
 import Link from "next/link";
 
-interface Lesson {
-  id: string;
-  title: string;
-  description: string;
-  tags: string[];
-  isRecommended: boolean;
+interface Prop {
+  userId: string;
 }
 
-const lessons: Lesson[] = [
-  {
-    id: "1",
-    title: "メールによる攻撃の対策",
-    description: "メールによる攻撃の対策について学びます。",
-    tags: ["基本的対策", "メール送受信"],
-    isRecommended: true,
-  },
-  {
-    id: "2",
-    title: "安全なパスワードの生成",
-    description: "安全なパスワードの生成の仕方について学びます。",
-    tags: ["基本的対策", "パスワード"],
-    isRecommended: true,
-  },
-  {
-    id: "3",
-    title: "ウイルス対策方法",
-    description: "ウイルス対策方法について学びます。",
-    tags: ["基本的対策", "ウイルス対策"],
-    isRecommended: false,
-  },
-  {
-    id: "4",
-    title: "不正アクセスの現状",
-    description: "不正アクセスの現状について学びます。",
-    tags: ["基本的対策", "アクセス制御"],
-    isRecommended: false,
-  },
-  {
-    id: "5",
-    title: "OSやソフトウェアの更新",
-    description: "OSやソフトウェアの更新の重要性について学びます。",
-    tags: ["基本的対策"],
-    isRecommended: false,
-  },
-];
-
-const LessonList: FC = () => {
+const LessonList: FC<Prop> = async ({ userId }) => {
+  const lessons = await serverApi.lessons.list({ userId });
   return (
     <div className="space-y-4">
       {lessons.map((lesson) => (
--- src/app/e-learning/page.tsx
+++ src/app/e-learning/page.tsx
@@ -1,12 +1,19 @@
 import { NextPage } from "next";
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 import LessonList from "../_components/e-learning/LessonList";
 
-const Elearning: NextPage = () => {
+const Elearning: NextPage = async () => {
+  const session = await getSession();
+  if (!session || !session.user.sub) {
+    redirect("/");
+  }
+
   return (
     <div className="container mx-auto p-4 max-w-4xl">
       <main>
         <h1 className="text-3xl font-bold mb-6">E-learning 一覧</h1>
-        <LessonList />
+        <LessonList userId={session.user.sub} />
       </main>
     </div>
   );
