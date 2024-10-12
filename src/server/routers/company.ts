19a3acf48816b751ff1a671334d592347537f614
T-Tommy0909 <tomi0909tomisawa@gmail.com>
2024-10-12 06:05:21 +0000

    feat: add fetching myIndustryResult api

--- src/server/routers/company.ts
+++ src/server/routers/company.ts
@@ -101,8 +101,126 @@ export const companyRouter = router({
         partScoreAverages: companyPartScoreAverages,
       };
     }),
+
+  myIndustryResult: procedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await prisma.user.findUnique({
+        where: { id: input.userId },
+        select: { company: { select: { industryId: true } } },
+      });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      if (!user.company || !user.company.industryId) {
+        throw new Error("User does not belong to any company or industry");
+      }
+
+      const industry = await prisma.industry.findUnique({
+        where: { id: user.company.industryId },
+        include: {
+          companies: { include: { users: { include: { answers: true } } } },
+        },
+      });
+      if (!industry) {
+        throw new Error("Industry not found");
+      }
+
+      // 予算を登録している会社の予算の平均値を取得
+      const validBudgets = industry.companies.flatMap((company) => {
+        if (!company.budget) {
+          return [];
+        }
+        return company.budget;
+      });
+      const industryBudgetAverage =
+        validBudgets.reduce((acc, curr) => acc + curr, 0) / validBudgets.length;
+
+      // 診断実施したユーザーがいる会社の回答の平均値を取得
+      const companyAnswerAverages = await Promise.all(
+        industry.companies.map(async (company) => {
+          const userAnswers = await Promise.all(
+            company.users.map(async (user) => {
+              if (user.answers.length === 0) return [];
+              return Promise.all(
+                user.answers.map(async (answer) => {
+                  const answerChoice = await prisma.answerChoice.findUnique({
+                    where: { id: answer.answerChoiceId },
+                    select: { points: true },
+                  });
+                  if (!answerChoice) {
+                    throw new Error("Answer choice not found");
+                  }
+                  return answerChoice.points;
+                }),
+              );
+            }),
+          );
+          // その会社で診断を実施したユーザーがいない場合は、空配列を返す
+          if (userAnswers.flat().length === 0) {
+            return [];
+          }
+
+          const companyAnswerAverage =
+            userAnswers.flat().reduce((acc, curr) => acc + curr, 0) /
+            userAnswers.flat().length;
+          return companyAnswerAverage;
+        }),
+      );
+      const averageScore =
+        companyAnswerAverages.flat().reduce((acc, curr) => acc + curr, 0) /
+        companyAnswerAverages.flat().length;
+
+      const parts = await prisma.part.findMany({
+        include: {
+          questions: { select: { id: true } },
+          categorys: {
+            include: {
+              answers: {
+                include: {
+                  user: {
+                    include: { company: { select: { industryId: true } } },
+                  },
+                  answerChoice: { select: { points: true } },
+                },
+              },
+            },
+          },
+        },
+      });
+
+      const industryPartScoreAverages = await Promise.all(
+        parts.map(async (part) => {
+          // カテゴリ内の回答から、会社の回答を抽出し、その平均得点を計算
+          // flatMapでcategorysを一次元配列に変換
+          const partScores = part.categorys.flatMap((category) =>
+            category.answers
+              .filter(
+                (answer) =>
+                  answer.user.company &&
+                  answer.user.company.industryId === industry.id,
+              )
+              .map((answer) => answer.answerChoice.points),
+          );
+          const partScoreAverage =
+            partScores.reduce((acc, curr) => acc + curr, 0) / partScores.length;
+          return partScoreAverage;
+        }),
+      );
+
+      return {
+        industry: industry.name,
+        budget: industryBudgetAverage,
+        averageScore: averageScore,
+        partScoreAverages: industryPartScoreAverages,
+      };
+    }),
 });
 
 export type MyCompanyResult = Awaited<
   ReturnType<typeof companyRouter.myCompanyResult>
+>;
+
+export type MyIndustryResult = Awaited<
+  ReturnType<typeof companyRouter.myIndustryResult>
 >;
