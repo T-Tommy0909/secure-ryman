572b88a01dca994281b993127524a02a83a802d9
T-Tommy0909 <tomi0909tomisawa@gmail.com>
2024-10-12 08:29:29 +0000

    feat: add fetching competitorIndustryResultList api

--- src/server/routers/company.ts
+++ src/server/routers/company.ts
@@ -1,5 +1,6 @@
 import { router, procedure } from "@/server/trpc";
 import { prisma } from "@/server/utils/prisma";
+import { Return } from "@prisma/client/runtime/library";
 import { z } from "zod";
 
 export const companyRouter = router({
@@ -215,6 +216,123 @@ export const companyRouter = router({
         partScoreAverages: industryPartScoreAverages,
       };
     }),
+
+  competitorIndustryResultList: procedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await prisma.user.findUnique({
+        where: { id: input.userId },
+        select: { companyId: true },
+      });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      if (!user.companyId) {
+        throw new Error("User does not belong to any company");
+      }
+
+      const myCompanyData = await prisma.company.findUnique({
+        where: { id: user.companyId },
+        select: { industryId: true },
+      });
+      if (!myCompanyData) {
+        throw new Error("Company not found");
+      }
+      if (!myCompanyData.industryId) {
+        throw new Error("Industry not found");
+      }
+
+      const competitorData = await prisma.company.findMany({
+        where: {
+          id: { not: user.companyId },
+          industryId: myCompanyData.industryId,
+        },
+        include: {
+          users: {
+            include: {
+              answers: true,
+            },
+          },
+        },
+      });
+      if (!competitorData) {
+        throw new Error("Company not found");
+      }
+
+      const parts = await prisma.part.findMany({
+        include: {
+          questions: { select: { id: true } },
+          categorys: {
+            include: {
+              answers: {
+                include: {
+                  user: { select: { companyId: true } },
+                  answerChoice: { select: { points: true } },
+                },
+              },
+            },
+          },
+        },
+      });
+
+      const competitorResults = await Promise.all(
+        competitorData.map(async (company) => {
+          const userAnswers = await Promise.all(
+            company.users.map(async (user) => {
+              if (user.answers.length === 0) return [];
+              return Promise.all(
+                user.answers.map(async (answer) => {
+                  const answerChoice = await prisma.answerChoice.findUnique({
+                    where: { id: answer.answerChoiceId },
+                    select: { points: true },
+                  });
+                  if (!answerChoice) {
+                    throw new Error("Answer choice not found");
+                  }
+                  return answerChoice.points;
+                }),
+              );
+            }),
+          );
+
+          const companyAnswerAverage =
+            userAnswers.flat().reduce((acc, curr) => acc + curr, 0) /
+            userAnswers.flat().length;
+
+          const companyPartScoreAverages = await Promise.all(
+            parts.map(async (part) => {
+              const partScores = part.categorys.flatMap((category) =>
+                category.answers
+                  .filter((answer) => answer.user.companyId === company.id)
+                  .map((answer) => answer.answerChoice.points),
+              );
+              const partScoreAverage =
+                partScores.reduce((acc, curr) => acc + curr, 0) /
+                partScores.length;
+              return partScoreAverage;
+            }),
+          );
+
+          const industry = await prisma.industry.findUnique({
+            where: { id: company.industryId! },
+            select: { name: true },
+          });
+          if (!industry) {
+            throw new Error("Industry not found");
+          }
+
+          return {
+            industry: industry.name,
+            employee: company.employee,
+            budget: company.budget,
+            averageScore: companyAnswerAverage,
+            partScoreAverages: companyPartScoreAverages,
+          };
+        }),
+      );
+
+      return competitorResults;
+    }),
 });
 
 export type MyCompanyResult = Awaited<
@@ -223,4 +341,8 @@ export type MyCompanyResult = Awaited<
 
 export type MyIndustryResult = Awaited<
   ReturnType<typeof companyRouter.myIndustryResult>
+>;
+
+export type CompetitorIndustryResultList = Awaited<
+  ReturnType<typeof companyRouter.competitorIndustryResultList>
 >;
