95639af5bc16c3ee3e159f13d9a230560dd302fc
T-Tommy0909 <tomi0909tomisawa@gmail.com>
2024-10-13 12:44:20 +0000

    feat: add fetching myAnswers api

--- src/server/routers/quiz.ts
+++ src/server/routers/quiz.ts
@@ -11,6 +11,43 @@ export const quizRouter = router({
         include: { quizAnswer: true },
       });
     }),
+
+  myAnswers: procedure
+    .input(z.object({ lessonId: z.string(), userId: z.string() }))
+    .query(async ({ input }) => {
+      const myAnswers = await prisma.quiz.findMany({
+        where: { lessonId: BigInt(input.lessonId) },
+        include: {
+          quizAnswer: {
+            where: { users: { some: { id: input.userId } } },
+          },
+        },
+      });
+      const quizWithAnswer = await prisma.quiz.findMany({
+        where: { lessonId: BigInt(input.lessonId) },
+        include: {
+          quizAnswer: {
+            where: { isCorrect: true },
+            select: { text: true },
+          },
+        },
+      });
+
+      // 問題と、その問題に対する回答をマージ
+      return myAnswers.map((myAnswer) => {
+        const quiz = quizWithAnswer.find((quiz) => quiz.id === myAnswer.id);
+        return {
+          quizText: myAnswer.text,
+          userAnswer: {
+            text: myAnswer.quizAnswer[0].text,
+            isCorrect: myAnswer.quizAnswer[0].isCorrect,
+          },
+          quizAnswer: quiz!.quizAnswer[0].text,
+        };
+      });
+    }),
 });
 
 export type QuizList = Awaited<ReturnType<typeof quizRouter.list>>;
+
+export type MyAnswers = Awaited<ReturnType<typeof quizRouter.myAnswers>>;
